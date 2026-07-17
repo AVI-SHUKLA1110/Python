5f449a7adc6a9da4b2261e0a741c94a83661358a
AVI-SHUKLA1110 <ashikla693@gmail.com>
2026-07-18 02:21:57 +0530

    .

--- oops.py
+++ oops.py
@@ -1,1 +1,13 @@
 #method - a function contained within a class 
+class Dog:
+    def __init__(self, name , breed , color):
+     self.name = name
+     self.breed = breed
+     self.color = color
+
+#objects for Dog class
+dog1 = Dog("albert" , "Grey hound" , "Black")
+dog2 = Dog("Rocky" , "German Shephard" , "Brown")
+
+print(dog1.color, dog1.breed)
+print(dog2.color, dog2.breed)
