87d3e254bfd4127aff07f24a049de0b61e55a5b9
AVI-SHUKLA1110 <ashikla693@gmail.com>
2026-07-18 20:07:40 +0530

    updated

--- oops.py
+++ oops.py
@@ -1,3 +1,4 @@
+#class - a blueprint for creating objects
 #method - a function contained within a class 
 class Dog:
     def __init__(self, name , breed , color):
@@ -11,3 +12,26 @@ dog2 = Dog("Rocky" , "German Shephard" , "Brown")
 
 print(dog1.color, dog1.breed)
 print(dog2.color, dog2.breed)
+
+
+class Owner:
+   def __init__(self, name , address , number):
+      self.name = name
+      self.address = address
+      self.number = number
+
+owner1 = Owner("aman" , "Gorakhpur" , "123")
+owner2 = Owner("harsh" , "Ludhiana" , "456")
+
+
+class Person:
+   def __init__(self, name, age) : 
+      self.name = name
+      self.age = age
+   def greet(self) :
+      print(f"Hello, my name is {self.name} and my age is {self.age}")
+
+person1 = Person("aman" , 26)
+person2 = Person("hercueles" , 30)
+
+
